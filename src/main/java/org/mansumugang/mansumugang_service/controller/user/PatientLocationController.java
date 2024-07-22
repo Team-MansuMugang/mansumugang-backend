f8d4c48c4d629407b3693e6369769571fc9fbae4
Jeong SeongHun <2000tjdgns@naver.com>
2024-07-22 13:17:27 +0900

    39번 이슈에 대한 팀장 의견 반영하여 코드 수정

--- src/main/java/org/mansumugang/mansumugang_service/controller/user/PatientLocationController.java
+++ src/main/java/org/mansumugang/mansumugang_service/controller/user/PatientLocationController.java
@@ -5,9 +5,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import org.mansumugang.mansumugang_service.domain.user.Patient;
-import org.mansumugang.mansumugang_service.domain.user.Protector;
+import org.mansumugang.mansumugang_service.domain.user.User;
 import org.mansumugang.mansumugang_service.dto.user.location.PatientLocationDto;
-import org.mansumugang.mansumugang_service.dto.user.location.PatientLocationInquiryRequestDto;
 import org.mansumugang.mansumugang_service.dto.user.location.PatientLocationRequestDto;
 import org.mansumugang.mansumugang_service.dto.user.location.PatientLocationResponseDto;
 import org.mansumugang.mansumugang_service.service.user.UserLocationService;
@@ -27,7 +26,7 @@ public class PatientLocationController {
     // 환자 현재 위치 정보 저장
     @PostMapping("/save")
     public ResponseEntity<PatientLocationResponseDto> saveUserLocation(
-            @AuthenticationPrincipal Patient patient,
+            @AuthenticationPrincipal User patient,
             @RequestBody PatientLocationRequestDto patientLocationRequestDto
     ){
         PatientLocationDto patientLocationDto = userLocationService.saveUserLocation(patient, patientLocationRequestDto);
@@ -36,12 +35,12 @@ public class PatientLocationController {
         return new ResponseEntity<>(PatientLocationResponseDto.DtoToResponse(patientLocationDto), HttpStatus.CREATED);
     }
 
-    @GetMapping("/user")
+    @GetMapping("/user/{patient_id}")
     public ResponseEntity<PatientLocationResponseDto> getUserLocation(
-            @AuthenticationPrincipal Protector protector,
-            @RequestBody PatientLocationInquiryRequestDto patientLocationInquiryRequestDto
+            @AuthenticationPrincipal User protector,
+            @PathVariable("patient_id") Long patientId
     ){
-        PatientLocationDto patientLocationDto = userLocationService.getUserLocation(protector, patientLocationInquiryRequestDto);
+        PatientLocationDto patientLocationDto = userLocationService.getUserLocation(protector, patientId);
 
         return new ResponseEntity<>(PatientLocationResponseDto.DtoToResponse(patientLocationDto), HttpStatus.OK);
     }
--- src/main/java/org/mansumugang/mansumugang_service/service/user/UserLocationService.java
+++ src/main/java/org/mansumugang/mansumugang_service/service/user/UserLocationService.java
@@ -7,17 +7,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.mansumugang.mansumugang_service.constant.ErrorType;
 import org.mansumugang.mansumugang_service.domain.user.Patient;
 import org.mansumugang.mansumugang_service.domain.user.Protector;
+import org.mansumugang.mansumugang_service.domain.user.User;
 import org.mansumugang.mansumugang_service.domain.user.UserLocation;
 import org.mansumugang.mansumugang_service.dto.user.location.PatientLocationDto;
-import org.mansumugang.mansumugang_service.dto.user.location.PatientLocationInquiryRequestDto;
 import org.mansumugang.mansumugang_service.dto.user.location.PatientLocationRequestDto;
 import org.mansumugang.mansumugang_service.exception.CustomErrorException;
 import org.mansumugang.mansumugang_service.repository.PatientRepository;
 import org.mansumugang.mansumugang_service.repository.ProtectorRepository;
 import org.mansumugang.mansumugang_service.repository.UserLocationRepository;
+import org.mansumugang.mansumugang_service.repository.UserRepository;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 import static org.mansumugang.mansumugang_service.constant.LocationBoundary.*;
 
@@ -27,40 +26,38 @@ import static org.mansumugang.mansumugang_service.constant.LocationBoundary.*;
 public class UserLocationService {
 
     private final UserLocationRepository userLocationRepository;
+    private final UserRepository userRepository;
     private final PatientRepository patientRepository;
     private final ProtectorRepository protectorRepository;
 
     @Transactional
-    public PatientLocationDto saveUserLocation(Patient patient, PatientLocationRequestDto patientLocationRequestDto ){
+    public PatientLocationDto saveUserLocation(User patient, PatientLocationRequestDto patientLocationRequestDto ){
         log.info("saveUserLocation 호출");
 
-        // 1. 요청으로 온 userId로 찾은 유저가 존재하는지 검증
-        Patient foundPatient = validatePatient(patient.getId());
-
-        // 2. 경도, 위도 유효성 검사(대한민국 안에 존재하는지 체크)
+        // 1. 경도, 위도 유효성 검사(대한민국 안에 존재하는지 체크)
         validateUserLocation(patientLocationRequestDto);
 
-        // 3. 경위도 정보 저장
-        UserLocation savedUserLocation = userLocationRepository.save(UserLocation.fromRequestDto(foundPatient, patientLocationRequestDto));
+        // 2. 경위도 정보 저장
+        UserLocation savedUserLocation = userLocationRepository.save(UserLocation.fromRequestDto((Patient) patient, patientLocationRequestDto));
         log.info("경위도 정보 저장 완료");
 
 
-        return PatientLocationDto.fromEntity(foundPatient,savedUserLocation);
+        return PatientLocationDto.fromEntity((Patient) patient,savedUserLocation);
     }
 
-    public PatientLocationDto getUserLocation(Protector protector, PatientLocationInquiryRequestDto patientLocationInquiryRequestDto){
+    public PatientLocationDto getUserLocation(User protector, Long patientId){
 
         // 1. 요청으로 온 userId로 찾은 유저가 존재하는지 검증
         log.info("환자, 보호자 유효성 검사 실시");
 
-        Patient foundPatient = validatePatient(patientLocationInquiryRequestDto.getPatientId());
-        Protector foundProtector = validateProtector(protector.getId()); // 현재 로그인 되있는 보호자의 정보를 검증하는게 의미가 있나(?)
+        Patient foundPatient = validatePatient(patientId);
+        validateProtector(protector); // protector 가 null 인지 검증
 
-        log.info("환자, 보호자 유효성 검사 완료. 환자 고유번호(user_id)={}, 보호자 고유번호(user_id)={}", foundPatient.getId(), foundProtector.getId());
+        log.info("환자, 보호자 유효성 검사 완료. 환자 고유번호(user_id)={}, 보호자 고유번호(user_id)={}", foundPatient.getId(), protector.getId());
 
         // 2. 요청으로 온 보호자의 엑세스 토큰으로 보호자의 환자 조회
         log.info("보호자와 환자 간 관계 유효성 검사 시작");
-        validateRelation(foundProtector, foundPatient);
+        checkUserIsProtectorOfPatient((Protector) protector, foundPatient);
 
 
         // 3. user_id로 찾아진 유저 마지막 위치 저장 시간순으로 내림차순 후 하나의 튜플 추출
@@ -74,42 +71,29 @@ public class UserLocationService {
 
     }
 
-    public Patient validatePatient(Long userId) {
-        log.info("userId로 환자 찾기 시작, userId={}", userId);
+    public Patient validatePatient(Long patientId) {
+        log.info("patientId로 환자 찾기 시작, patientId={}", patientId);
 
-        return patientRepository.findById(userId)
+        return patientRepository.findById(patientId)
                 .orElseThrow(() -> {
-                    log.error("userId로 찾은 환자가 존재하지 않음, userId={}", userId);
+                    log.error("userId로 찾은 환자가 존재하지 않음, userId={}", patientId);
                     return new CustomErrorException(ErrorType.UserNotFoundError);
                 });
     }
 
-    public Protector validateProtector(Long userId) {
-        log.info("userId로 보호자 찾기 시작, userId={}", userId);
-
-        return protectorRepository.findById(userId)
-                .orElseThrow(() -> {
-                    log.error("userId로 찾은 보호자가 존재하지 않음, userId={}", userId);
-                    return new CustomErrorException(ErrorType.UserNotFoundError);
-                });
-    }
-
-    private void validateRelation(Protector foundProtector, Patient foundPatient) {
-
-        log.info("보호자의 고유번호(user_id)로 환자 보호자의 환자 명단 조회 시작");
-        List<Patient> patientList = patientRepository.findByProtector_id(foundProtector.getId());
-        log.info("보호자의 환자 명단 조회 결과 ={}명", (long) patientList.size());
-
-        // 보호자가 관리하는 환자 목록에 요청한 환자가 포함되어 있는지 검증
-        log.info("환자 보호자 관계 검증 시작.");
-        boolean isPatientManagedByProtector = patientList.stream()
-                .anyMatch(patient -> patient.getId().equals(foundPatient.getId()));
-
-        if (!isPatientManagedByProtector) {
-            throw new CustomErrorException(ErrorType.RelationDismatchError);  // 보호자가 해당 환자를 관리하지 않는 경우
+    public void validateProtector(User protector) {
+        if (protector == null) {
+            throw new CustomErrorException(ErrorType.UserNotFoundError);
         }
+    }
 
-        log.info("환자 보호자 관계 검증 완료.");
+    private void checkUserIsProtectorOfPatient(Protector protector, Patient patient) {
+
+        log.info("유저가 환자의 보호자인지 검증 시작");
+        if(!patient.getProtector().getUsername().equals(protector.getUsername())) {
+            throw new CustomErrorException(ErrorType.AccessDeniedError);
+        }
+        log.info("유저가 환자의 보호자인지 검증 완료");
     }
 
 
