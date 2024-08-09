932f60052aa94adb2d5601b49bea747423c1f7fa
CHOI-MIN-SU <mine52100@naver.com>
2024-08-09 17:05:56 +0900

    feat: 특정일 약 조회 시 병원까지 같이 조회되도록 구현 및 스펙 변경

--- src/main/java/org/mansumugang/mansumugang_service/repository/HospitalRepository.java
+++ src/main/java/org/mansumugang/mansumugang_service/repository/HospitalRepository.java
@@ -1,7 +1,12 @@
 package org.mansumugang.mansumugang_service.repository;
 
 import org.mansumugang.mansumugang_service.domain.hospital.Hospital;
+import org.mansumugang.mansumugang_service.domain.user.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface HospitalRepository extends JpaRepository<Hospital, Long> {
+    List<Hospital> findAllByPatientIdAndHospitalVisitingTimeBetween(Long patientId, LocalDateTime hospitalVisitingTime, LocalDateTime hospitalVisitingTime2);
 }
